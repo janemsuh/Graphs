b2d00d7e0ed0fe93fe91a68a81a31d68f854b764
Jane Suh <sourcherryyogurt@gmail.com>
2020-10-26 00:37:08 -0400

    Completed Day 2: Earliest Ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,30 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    ancestors_hash = {}
+
+    for value, key in ancestors:
+        if key in ancestors_hash:
+            ancestors_hash[key].add(value)
+        else:
+            ancestors_hash[key] = set([value])
+
+    if starting_node not in ancestors_hash:
+        return -1
+
+    stack = [starting_node]
+    visited = set()
+    earliest_ancestor = None
+
+    while len(stack) > 0:
+        current_node = stack.pop()
+
+        if current_node not in visited:
+            visited.add(current_node)
+
+            if current_node in ancestors_hash:
+                for ancestor in ancestors_hash[current_node]:
+                    if ancestor not in ancestors_hash:
+                        earliest_ancestor = ancestor
+                    stack.append(ancestor)
+
+    return earliest_ancestor
